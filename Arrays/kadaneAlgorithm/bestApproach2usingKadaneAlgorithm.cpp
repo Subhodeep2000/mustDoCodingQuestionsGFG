8362ab45de220c681e77079b757a55d52537077d
Subhodeep Kundu <kundu.subhodeep2000@gmail.com>
2020-09-11 23:27:08 +0530

    Kadane Algorithm best approach 2 added

---
+++ Arrays/kadaneAlgorithm/bestApproach2usingKadaneAlgorithm.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int*A = new int[n];
+		for (int i = 0; i < n; i++) {
+			cin >> A[i];
+		}
+		int maxm = A[0];
+		int sum = 0;
+		for (int i = 0; i <= n - 1; i++) {
+			sum += A[i];
+			maxm = max(maxm, sum);
+			if (sum < 0) sum = 0;
+		}
+		if (maxm == 0)cout << "-1" << endl;
+		else cout << maxm << endl;
+	}
+	return 0;
+}
